8e7a40ca836f7f506b02f1beb8f485e6c962ce9a
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2016-02-03 21:26:17 -0200

    Major rewrite to FalconPunch, rename it to FalconPuncher

---
+++ FalconPuncher.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+from __future__ import unicode_literals, print_function, division
+
+import argparse
+import os
+import sys
+import struct
+import time
+
+from socket import socket
+from contextlib import closing
+
+try:
+    input = raw_input
+except NameError:
+    pass
+
+FBI_PORT = 5000
+CHUNK_SIZE = 32786
+KB = 1024
+WAIT_TIME = 1.5
+
+def send_file(filename, dest_ip):
+    statinfo = os.stat(filename)
+    fbiinfo = struct.pack("!q", statinfo.st_size)
+
+    with open(filename, "rb") as f:
+        with closing(socket()) as sock:
+            try:
+                sock.connect((dest_ip, FBI_PORT))
+            except ConnectionRefusedError:
+                sys.exit("Connection error with IP {}. Is FBI running?"
+                        .format(dest_ip))
+            try:
+                sock.send(fbiinfo)
+                bytes_transferred = 0
+                while True:
+                    chunk = f.read(CHUNK_SIZE)
+                    if not chunk:
+                        break # EOF
+                    bytes_transferred += sock.send(chunk)
+                    sys.stdout.write("\rProgress: {:.2f}KB of {:.2f}KB"
+                            .format(bytes_transferred / KB, statinfo.st_size / KB))
+                sys.stdout.write("\n")
+            except ConnectionResetError:
+                sys.exit("\nConnection closed by FBI. Check FBI for errors.")
+
+def argparser():
+    parser = argparse.ArgumentParser(description="Send CIA files to FBI via network.")
+    parser.add_argument("file", nargs="+", help="CIA file to send to FBI")
+    parser.add_argument("-i", "--ip", help="IP from target 3DS")
+    return parser
+
+def main():
+    parser = argparser()
+    args = parser.parse_args()
+    if args.ip:
+        dest_ip = args.ip
+    else:
+        dest_ip = input("Enter IP: ")
+    for filename in args.file:
+        if os.path.isfile(filename):
+            print("Sending file {}".format(filename))
+            time.sleep(WAIT_TIME)
+            send_file(filename, dest_ip)
+        else:
+            sys.exit("{} is not a file.".format(filename))
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
---
+++ README.md
@@ -0,0 +1,28 @@
+FalconPuncher
+=============
+
+Send CIA files to FBI via network
+---------------------------------
+
+This is a fork of [blockfeed/FalconPunch](https://github.com/blockfeed/FalconPunch.git) with support for Python 3 (Python 2 is still supported), better user interface (still completely command line-based) and better error handling. This program can be used with a 3DS with [Steveice10/FBI](https://github.com/Steveice10/FBI) installed to transfer CIA files to 3DS using network instead of manually copying files to SD card. This is somewhat slowever, however it has the advantage of not using double the space to install.
+
+Usage
+-----
+
+FalconPuncher does not have any requirements besides a basic Python 2/3 installation. You can just download it ([direct link](https://raw.githubusercontent.com/m45t3r/FalconPuncher/master/FalconPuncher.py)) and run:
+
+    $ python FalconPuncher.py -h
+
+This will show the program help. To transfer a CIA to 3DS:
+
+    $ python FalconPuncher.py some/cia/file.cia
+
+The program will ask target 3DS IP before transfering the file. You can skip this step (useful for scripts or something) by passing `-i` option:
+
+    $ python FalconPuncher.py -i 192.168.1.5 /some/cia/file.cia # change "192.168.1.5" to the IP of your 3DS!
+
+You can pass multiple CIA files too:
+
+    $ python FalconPuncher.py -i 192.168.1.5 /some/cia/file.cia /another/cia/file.cia
+
+It is tested in Python 3.5.1, however it should work from Python 2.6 and above, and from Python 3.2 and above. At least I hope so.
