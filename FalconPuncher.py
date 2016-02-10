d0f1c6ec885ce3ceaf6b635c5b2bf5aa8e6a3971
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2016-02-10 16:35:37 -0200

    Make Tk requirement optional
    Some distros does not install it by default (Arch Linux, Gentoo), giving
    strange errors and not allowing it to work even in pure command-line
    mode.

--- FalconPuncher.py
+++ FalconPuncher.py
@@ -11,14 +11,20 @@ import struct
 import time
 from contextlib import closing
 
+GUI_ENABLED = True
 try:
     import tkinter as tk
     from tkinter import messagebox as tkMessageBox
     from tkinter import ttk
 except ImportError:
-    import Tkinter as tk
-    import tkMessageBox
-    import ttk
+    try:
+        import Tkinter as tk
+        import tkMessageBox
+        import ttk
+    except ImportError:
+        GUI_ENABLED = False
+        print("Tk is not installed in your system, GUI unavailable.",
+                file=sys.stderr)
 
 try:
     input = raw_input
@@ -31,113 +37,119 @@ KB = 1024
 CHUNK_SIZE = 128 * KB
 WAIT_TIME = 2
 
-class GUI(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-        self.title("FalconPuncher")
-        self.geometry("400x600")
+if GUI_ENABLED:
+    class GUI(tk.Tk):
+        def __init__(self, *args, **kwargs):
+            tk.Tk.__init__(self, *args, **kwargs)
+            self.title("FalconPuncher")
+            self.geometry("400x600")
 
-        # Create a listbox to show user filelist
-        self.lbl_filelist = tk.Label(
-                text="CIA list (double click to add to queue):",
-                anchor=tk.W)
-        self.lbl_filelist.pack(side=tk.TOP, fill=tk.X)
-        self.lb_filelist = tk.Listbox(selectmode=tk.SINGLE)
-        self.lb_filelist.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-        self.populate_filelist("*.cia")
-        self.lb_filelist.bind("<Double-Button-1>", self.add_file_to_sendlist)
-        # Create a scrollbar to allow filelist to an arbitrary number of files
-        self.sb_filelist = tk.Scrollbar(self.lb_filelist, orient=tk.VERTICAL)
-        self.sb_filelist.pack(side=tk.RIGHT, fill=tk.Y)
-        # Bind listbox and scrollbar together
-        self.sb_filelist.configure(command=self.lb_filelist.yview)
-        self.lb_filelist.configure(yscrollcommand=self.sb_filelist.set)
+            # Create a listbox to show user filelist
+            self.lbl_filelist = tk.Label(
+                    text="CIA list (double click to add to queue):",
+                    anchor=tk.W)
+            self.lbl_filelist.pack(side=tk.TOP, fill=tk.X)
+            self.lb_filelist = tk.Listbox(selectmode=tk.SINGLE)
+            self.lb_filelist.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+            self.populate_filelist("*.cia")
+            self.lb_filelist.bind("<Double-Button-1>",
+                    self.add_file_to_sendlist)
+            # Create a scrollbar
+            self.sb_filelist = tk.Scrollbar(self.lb_filelist,
+                    orient=tk.VERTICAL)
+            self.sb_filelist.pack(side=tk.RIGHT, fill=tk.Y)
+            # Bind listbox and scrollbar together
+            self.sb_filelist.configure(command=self.lb_filelist.yview)
+            self.lb_filelist.configure(yscrollcommand=self.sb_filelist.set)
 
-        # Create a listbox to show user the files selected to send
-        self.lbl_sendlist = tk.Label(
-                text="CIA's in queue (double click to remove from queue):",
-		anchor=tk.W)
-        self.lbl_sendlist.pack(side=tk.TOP, fill=tk.X, expand=False)
-        self.lb_sendlist = tk.Listbox(selectmode=tk.SINGLE)
-        self.lb_sendlist.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-        self.lb_sendlist.bind("<Double-Button-1>", self.remove_file_from_sendlist)
-        # Create a scrollbar to allow filelist to an arbitrary number of files
-        self.sb_sendlist = tk.Scrollbar(self.lb_sendlist, orient=tk.VERTICAL)
-        self.sb_sendlist.pack(side=tk.RIGHT, fill=tk.Y)
-        # Bind listbox and scrollbar together
-        self.sb_sendlist.configure(command=self.lb_sendlist.yview)
-        self.lb_sendlist.configure(yscrollcommand=self.sb_sendlist.set)
+            # Create a listbox to show user the files selected to send
+            self.lbl_sendlist = tk.Label(
+                    text="CIA's in queue (double click to remove from queue):",
+                    anchor=tk.W)
+            self.lbl_sendlist.pack(side=tk.TOP, fill=tk.X, expand=False)
+            self.lb_sendlist = tk.Listbox(selectmode=tk.SINGLE)
+            self.lb_sendlist.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+            self.lb_sendlist.bind("<Double-Button-1>",
+                    self.remove_file_from_sendlist)
+            # Create a scrollbar
+            self.sb_sendlist = tk.Scrollbar(self.lb_sendlist,
+                    orient=tk.VERTICAL)
+            self.sb_sendlist.pack(side=tk.RIGHT, fill=tk.Y)
+            # Bind listbox and scrollbar together
+            self.sb_sendlist.configure(command=self.lb_sendlist.yview)
+            self.lb_sendlist.configure(yscrollcommand=self.sb_sendlist.set)
 
-        # Textbox to allow user to change IP
-        self.lbl_ip = tk.Label(text="IP (press Y in FBI to show 3DS IP):",
-                anchor=tk.W)
-        self.lbl_ip.pack(side=tk.TOP, fill=tk.X)
-        self.ip = tk.StringVar()
-        self.ip.set(DEFAULT_IP)
-        self.ent_ip = tk.Entry(textvariable=self.ip)
-        self.ent_ip.pack(side=tk.TOP, fill=tk.X)
+            # Textbox to allow user to change IP
+            self.lbl_ip = tk.Label(text="IP (press Y in FBI to show 3DS IP):",
+                    anchor=tk.W)
+            self.lbl_ip.pack(side=tk.TOP, fill=tk.X)
+            self.ip = tk.StringVar()
+            self.ip.set(DEFAULT_IP)
+            self.ent_ip = tk.Entry(textvariable=self.ip)
+            self.ent_ip.pack(side=tk.TOP, fill=tk.X)
 
-        # Button to start file transferring
-        self.btn_start = tk.Button(text="Send", command=self.start_transfer)
-        self.btn_start.pack(side=tk.TOP, fill=tk.X)
+            # Button to start file transferring
+            self.btn_start = tk.Button(text="Send", command=self.start_transfer)
+            self.btn_start.pack(side=tk.TOP, fill=tk.X)
 
-        # Progress bar for send progress
-        self.prg_send = ttk.Progressbar(orient="horizontal", mode="determinate")
-        self.prg_send.pack(side=tk.TOP, fill=tk.X)
+            # Progress bar for send progress
+            self.prg_send = ttk.Progressbar(orient="horizontal",
+                    mode="determinate")
+            self.prg_send.pack(side=tk.TOP, fill=tk.X)
 
-    def populate_filelist(self, regex="/*"):
-        for filename in sorted(glob.glob(regex)):
-            self.lb_filelist.insert(tk.END, filename)
+        def populate_filelist(self, regex="/*"):
+            for filename in sorted(glob.glob(regex)):
+                self.lb_filelist.insert(tk.END, filename)
 
-    def get_files_from_filelist(self):
-        return self.lb_filelist.get(0, tk.END)
+        def get_files_from_filelist(self):
+            return self.lb_filelist.get(0, tk.END)
 
-    def get_files_from_sendlist(self):
-        return self.lb_sendlist.get(0, tk.END)
+        def get_files_from_sendlist(self):
+            return self.lb_sendlist.get(0, tk.END)
 
-    def add_file_to_sendlist(self, event):
-        position = self.lb_filelist.curselection()[0]
-        selection = self.lb_filelist.get(position)
-        if selection not in self.get_files_from_sendlist():
-            self.lb_sendlist.insert(tk.END, selection)
+        def add_file_to_sendlist(self, event):
+            position = self.lb_filelist.curselection()[0]
+            selection = self.lb_filelist.get(position)
+            if selection not in self.get_files_from_sendlist():
+                self.lb_sendlist.insert(tk.END, selection)
 
-    def remove_file_from_sendlist(self, event):
-        position = self.lb_sendlist.curselection()[0]
-        self.lb_sendlist.delete(position)
+        def remove_file_from_sendlist(self, event):
+            position = self.lb_sendlist.curselection()[0]
+            self.lb_sendlist.delete(position)
 
-    def start_transfer(self):
-        if not self.get_files_from_sendlist():
-            tkMessageBox.showerror("Error", "No files selected")
-            return
+        def start_transfer(self):
+            if not self.get_files_from_sendlist():
+                tkMessageBox.showerror("Error", "No files selected")
+                return
 
-        dest_ip = self.ip.get()
-        if not dest_ip:
-            tkMessageBox.showerror("Error", "Did not set an IP")
-            return
-        if not valid_ip(dest_ip):
-            tkMessageBox.showerror("Error", "Invalid IP")
-            return
+            dest_ip = self.ip.get()
+            if not dest_ip:
+                tkMessageBox.showerror("Error", "Did not set an IP")
+                return
+            if not valid_ip(dest_ip):
+                tkMessageBox.showerror("Error", "Invalid IP")
+                return
 
-        try:
-            for i, filename in enumerate(self.get_files_from_sendlist()):
-                for progress in send_file(filename, dest_ip):
-                    self.prg_send.step(progress)
+            try:
+                for i, filename in enumerate(self.get_files_from_sendlist()):
+                    for progress in send_file(filename, dest_ip):
+                        self.prg_send.step(progress)
+                        self.update_idletasks()
+                    self.lb_sendlist.delete(0)
                     self.update_idletasks()
-                self.lb_sendlist.delete(0)
-                self.update_idletasks()
-                time.sleep(WAIT_TIME)
-        except ConnectionRefusedError:
-            tkMessageBox.showerror("Error",
-                    "Connection error. Is FBI running?")
-        except ConnectionResetError:
-            tkMessageBox.showerror("Error",
-                    "Connection closed by FBI. Check FBI for errors.")
-        except OSError:
-            tkMessageBox.showerror("Error",
-                    "No route to host. Is IP correct?")
-        finally:
-            self.prg_send.config(value=0)
-            return
+                    time.sleep(WAIT_TIME)
+            except ConnectionRefusedError:
+                tkMessageBox.showerror("Error",
+                        "Connection error. Is FBI running?")
+            except ConnectionResetError:
+                tkMessageBox.showerror("Error",
+                        "Connection closed by FBI. Check FBI for errors.")
+            except OSError:
+                tkMessageBox.showerror("Error",
+                        "No route to host. Is IP correct?")
+            finally:
+                self.prg_send.config(value=0)
+                return
 
 def send_file(filename, dest_ip):
     statinfo = os.stat(filename)
@@ -177,29 +189,34 @@ def valid_ip(ip):
         return False
 
 def argparser():
-    parser = argparse.ArgumentParser(description="Send CIA files to FBI via network.")
+    parser = argparse.ArgumentParser(description=
+            "Send CIA files to FBI via network.")
     parser.add_argument("file", nargs="*", help="CIA file to send to FBI")
     parser.add_argument("-i", "--ip", help="IP from target 3DS")
-    parser.add_argument("--gui", action="store_true", help="force GUI mode")
+    if GUI_ENABLED:
+        parser.add_argument("--gui", action="store_true", help="force GUI mode")
     return parser
 
 def main():
     parser = argparser()
     args = parser.parse_args()
 
-    if args.gui or not args.file:
+    if GUI_ENABLED and (args.gui or not args.file):
         gui = GUI()
         gui.mainloop()
         sys.exit()
+
+    if not args.file:
+        sys.exit("Need to pass at least one file in command-line mode.")
+
+    for filename in args.file:
+        if not os.path.isfile(filename):
+            sys.exit("{} not found or is not a file.".format(filename))
 
     if args.ip:
         dest_ip = args.ip
     else:
         dest_ip = input("Enter IP: ")
-
-    for filename in args.file:
-        if not os.path.isfile(filename):
-            sys.exit("{} not found or is not a file.".format(filename))
 
     if not valid_ip(dest_ip):
         sys.exit("IP {} is invalid.".format(dest_ip))
